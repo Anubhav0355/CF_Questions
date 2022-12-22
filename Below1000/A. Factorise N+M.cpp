19de36d58bf5dd3aa82502bfbb1e4f906bcf931f
Anubhav0355 <anubhavsingh0355@gmail.com>
2022-12-23 03:54:29 +0530

    .

---
+++ Below1000/A. Factorise N+M.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define fo(i,n) for(int i=0;i<n;i++)
+#define pb push_back
+#define mp make_pair
+#define all(x) x.begin(), x.end()
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pii> vpii;
+typedef vector<pl> vpl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+
+int main() {
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    
+    int t;
+    cin>>t;
+
+    while(t--) {
+        int n;
+        cin>>n;
+
+        //if n is a prime number, then let's take m = n, this way m+n will not be a prime
+        cout<<n<<"\n";
+
+    }
+
+    return 0;
+}
