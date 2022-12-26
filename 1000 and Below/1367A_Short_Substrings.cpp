1bd38032453a4346857e07ff69cc32cfc9422684
Anubhav0355 <anubhavsingh0355@gmail.com>
2022-12-26 20:16:11 +0530

    Questions

---
+++ 1000 and Below/1367A_Short_Substrings.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define fo(i,n) for(int i=0;i<n;i++)
+#define pb push_back
+#define mp make_pair
+#define all(x) x.begin(), x.end()
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pii> vpii;
+typedef vector<pl> vpl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+
+int main() {
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    
+    int t;
+    cin>>t;
+
+    while(t--) {
+        string str,result;
+        cin>>str;
+
+        result.append(str.substr(0,2));
+
+        for(int i=3; i<str.size(); i+=2) {
+            result.pb(str[i]);
+        }
+
+        cout<<result<<"\n";
+
+    }
+
+    return 0;
+}
