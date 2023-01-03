ab4f322737eca5e81df1b805733de3d4e61a5b43
Anubhav0355 <anubhavsingh0355@gmail.com>
2023-01-03 22:25:37 +0530

    Questions

---
+++ 1000 and Below/1692A_Marathon.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define fo(i,n) for(int i=0;i<n;i++)
+#define pb push_back
+#define mp make_pair
+#define all(x) x.begin(), x.end()
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pii> vpii;
+typedef vector<pl> vpl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+
+int main() {
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    
+    int t;
+    cin>>t;
+
+    while(t--) {
+        int a,b,c,d;
+        cin>>a>>b>>c>>d;
+
+        int count = 0;
+        if(b > a) count++;
+        if(c > a) count++;
+        if(d > a) count++;
+
+        cout<<count<<"\n";
+
+    }
+
+    return 0;
+}
