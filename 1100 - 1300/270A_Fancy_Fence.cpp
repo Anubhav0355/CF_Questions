c56c1e0f00710dd907fd8aa55620093ae0f13006
Anubhav0355 <anubhavsingh0355@gmail.com>
2023-01-09 14:08:47 +0530

    Questions

---
+++ 1100 - 1300/270A_Fancy_Fence.cpp
@@ -0,0 +1,59 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define fo(i,n) for(int i=0;i<n;i++)
+#define pb push_back
+#define mp make_pair
+#define all(x) x.begin(), x.end()
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pii> vpii;
+typedef vector<pl> vpl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+
+int main() {
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    
+    int t;
+    cin>>t;
+
+    while(t--) {
+        int a;
+        cin>>a;
+
+        float angle = 0;
+        float i=3;
+        bool isFound = false;
+
+        while(true) {
+            angle = ((i-2)*180)/i;
+
+            if(angle == a) {
+                isFound = true;
+                break;
+            }
+            else if(angle > a) {
+                break;
+            }
+
+            i++;
+
+        }
+
+        if(isFound) {
+            cout<<"YES\n";
+        }
+        else {
+            cout<<"NO\n";
+        }
+
+    }
+
+    return 0;
+}
