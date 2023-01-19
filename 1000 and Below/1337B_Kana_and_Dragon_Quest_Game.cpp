ed16442fd0f5f7013a9f3859e92814461d20df41
Anubhav0355 <anubhavsingh0355@gmail.com>
2023-01-19 23:11:43 +0530

    Questions

---
+++ 1000 and Below/1337B_Kana_and_Dragon_Quest_Game.cpp
@@ -0,0 +1,65 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define fo(i,n) for(int i=0;i<n;i++)
+#define pb push_back
+#define mp make_pair
+#define all(x) x.begin(), x.end()
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pii> vpii;
+typedef vector<pl> vpl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+
+int main() {
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    
+    int t;
+    cin>>t;
+
+    while(t--) {
+        int x,n,m;
+        cin>>x>>n>>m;
+
+        int previousX = 0;
+
+        if(x <= 10 && m >= 1) {
+            cout<<"YES\n";
+            continue;
+        }
+
+        while(n-- && x > 0) {
+            x = x/2 + 10;
+            if(previousX == x) {
+                break;
+            }
+            else {
+                previousX = x;
+            }
+        }
+
+        //now either n became zero or x was repeating
+        //so now use all lightning strikes
+
+        while(m-- && x > 0) {
+            x -= 10;
+        }
+
+        if(x > 0) {
+            cout<<"NO\n";
+        }
+        else {
+            cout<<"YES\n";
+        }
+
+
+    }
+
+    return 0;
+}
