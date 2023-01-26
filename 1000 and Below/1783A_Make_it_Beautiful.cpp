9c4e6fe73ac6e211d230a1f742c87f0073572c31
Anubhav0355 <anubhavsingh0355@gmail.com>
2023-01-26 23:37:20 +0530

    Questions

---
+++ 1000 and Below/1783A_Make_it_Beautiful.cpp
@@ -0,0 +1,55 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define fo(i,n) for(int i=0;i<n;i++)
+#define pb push_back
+#define mp make_pair
+#define all(x) x.begin(), x.end()
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pii> vpii;
+typedef vector<pl> vpl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+
+int main() {
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    
+    int t;
+    cin>>t;
+
+    while(t--) {
+        int n;
+        cin>>n;
+        int arr[n];
+
+        fo(i,n) {
+            cin>>arr[i];
+        }
+
+        sort(arr,arr+n);
+
+        int max = arr[n-1];
+        int min = arr[0];
+
+        if(max == min) {
+            cout<<"NO\n";
+        }
+        else {
+            cout<<"YES\n";
+            cout<<arr[n-1]<<" "<<arr[0]<<" ";
+            for(int i=1; i<n-1; i++) {
+                cout<<arr[i]<<" ";
+            }
+            cout<<"\n";
+        }
+
+    }
+
+    return 0;
+}
