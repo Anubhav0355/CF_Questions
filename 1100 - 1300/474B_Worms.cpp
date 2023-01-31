a77be222618046c66c442cd9fde57a33b776678b
Anubhav0355 <anubhavsingh0355@gmail.com>
2023-01-31 23:16:16 +0530

    Questions

---
+++ 1100 - 1300/474B_Worms.cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define fo(i,n) for(int i=0;i<n;i++)
+#define pb push_back
+#define mp make_pair
+#define all(x) x.begin(), x.end()
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pii> vpii;
+typedef vector<pl> vpl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+
+int main() {
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    
+    int n;
+    cin>>n;
+
+    int arr[n+1];
+    for(int i=1; i<=n; i++) {
+        cin>>arr[i];
+    }
+
+    int preSum[n+1];
+    preSum[1] = arr[1];
+
+    for(int i=2; i<=n; i++) {
+        preSum[i] = preSum[i-1] + arr[i];
+    }
+
+    int m;
+    cin>>m;
+
+    while(m--) {
+        int q;
+        cin>>q;
+
+        if(q <= preSum[1]) {
+            cout<<1<<"\n";
+            continue;
+        }
+
+        auto ans = lower_bound(preSum,preSum+n+1,q) - preSum;
+        cout<<ans<<"\n";
+
+    }
+
+    return 0;
+}
