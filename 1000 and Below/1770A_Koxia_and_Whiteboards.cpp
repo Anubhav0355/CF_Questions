4f21ac38394a7fd3fa773f9f8302a03faa4af82c
Anubhav0355 <anubhavsingh0355@gmail.com>
2023-02-11 11:29:50 +0530

    Question

---
+++ 1000 and Below/1770A_Koxia_and_Whiteboards.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define fo(i,n) for(int i=0;i<n;i++)
+#define pb push_back
+#define mp make_pair
+#define all(x) x.begin(), x.end()
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pii> vpii;
+typedef vector<pl> vpl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+
+int main() {
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    
+    int t;
+    cin>>t;
+
+    while(t--) {
+        int n,m;
+        cin>>n>>m;
+
+        int arr[n];
+        fo(i,n) {
+            cin>>arr[i];
+        }
+
+        fo(i,m) {
+            sort(arr,arr+n);
+            int num;
+            cin>>num;
+            swap(arr[0],num);
+        }
+
+        long long sum = 0;
+        fo(i,n) {
+            sum+=arr[i];
+        }
+
+        cout<<sum<<"\n";
+
+
+    }
+
+    return 0;
+}
