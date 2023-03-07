9f5345421dedce50a7a500124d60382686d05ad0
Anubhav0355 <anubhavsingh0355@gmail.com>
2023-03-08 01:32:13 +0530

    Question

---
+++ 1000 and Below/1371A_Magical_Sticks.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define fo(i,n) for(int i=0;i<n;i++)
+#define pb push_back
+#define mp make_pair
+#define all(x) x.begin(), x.end()
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pii> vpii;
+typedef vector<pl> vpl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+
+int main() {
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    
+    int t;
+    cin>>t;
+
+    while(t--) {
+        int n;
+        cin>>n;
+
+        if(n&1) {
+            cout<<n/2 + 1<<"\n";
+        }
+        else {
+            cout<<n/2<<"\n";
+        }
+
+    }
+
+    return 0;
+}
