71793cba771b68e5153a0de8f3f99698c2621610
Anubhav0355 <anubhavsingh0355@gmail.com>
2023-03-09 12:24:17 +0530

    Question

---
+++ 1000 and Below/1389A_LCM_Problem.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define fo(i,n) for(int i=0;i<n;i++)
+#define pb push_back
+#define mp make_pair
+#define all(x) x.begin(), x.end()
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pii> vpii;
+typedef vector<pl> vpl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+
+int main() {
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    
+    int t;
+    cin>>t;
+
+    while(t--) {
+        ll l,r;
+        cin>>l>>r;
+
+        if(r >= 2*l) {
+            cout<<l<<" "<<2*l;
+        }
+        else {
+            cout<<-1<<" "<<-1;
+        }
+        cout<<"\n";
+    }
+
+    return 0;
+}
