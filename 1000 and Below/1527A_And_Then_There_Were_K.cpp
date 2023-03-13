9114f75b2e2080caade6b728d5ebb375f855077e
Anubhav0355 <anubhavsingh0355@gmail.com>
2023-03-14 01:35:20 +0530

    Question

---
+++ 1000 and Below/1527A_And_Then_There_Were_K.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define fo(i,n) for(int i=0;i<n;i++)
+#define pb push_back
+#define mp make_pair
+#define all(x) x.begin(), x.end()
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pii> vpii;
+typedef vector<pl> vpl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+
+int main() {
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    
+    int t;
+    cin>>t;
+
+    while(t--) {
+        float n;
+        cin>>n;
+
+        int bits = log2(n) + 1;
+        cout<<(1<<(bits-1))-1<<"\n";
+
+    }
+
+    return 0;
+}
