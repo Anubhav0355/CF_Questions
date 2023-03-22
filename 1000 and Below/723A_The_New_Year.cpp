8289cb13206f531fa1d5d60a57894928fba30ae9
Anubhav0355 <anubhavsingh0355@gmail.com>
2023-03-23 01:48:06 +0530

    Question

---
+++ 1000 and Below/723A_The_New_Year.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define fo(i,n) for(int i=0;i<n;i++)
+#define pb push_back
+#define mp make_pair
+#define all(x) x.begin(), x.end()
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pii> vpii;
+typedef vector<pl> vpl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+
+int main() {
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    
+    int arr[3];
+    fo(i,3) {
+        cin>>arr[i];
+    }
+
+    sort(arr,arr+3);
+
+    cout<<abs(arr[0] - arr[1]) + abs(arr[1] - arr[2]);
+
+    return 0;
+}
