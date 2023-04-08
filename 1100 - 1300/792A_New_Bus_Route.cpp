0e7b9fbb88e7d43995ff2dd54275f904e71e6752
Anubhav0355 <anubhavsingh0355@gmail.com>
2023-04-08 17:07:51 +0530

    Question

---
+++ 1100 - 1300/792A_New_Bus_Route.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define fo(i,n) for(int i=0;i<n;i++)
+#define pb push_back
+#define mp make_pair
+#define all(x) x.begin(), x.end()
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pii> vpii;
+typedef vector<pl> vpl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+
+int main() {
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    
+    int n;
+    cin>>n;
+
+    int arr[n];
+    fo(i,n) {
+        cin>>arr[i];
+    }
+
+    sort(arr,arr+n);
+
+    map<int,int> mp;
+
+    fo(i,n-1) {
+        mp[abs(arr[i] - arr[i+1])]++;
+    }
+
+    for(auto x : mp) {
+        cout<<x.first<<" "<<x.second;
+        break;
+    }
+
+    return 0;
+}
