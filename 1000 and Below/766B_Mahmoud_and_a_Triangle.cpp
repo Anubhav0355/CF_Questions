9cb95f56cc1883e753ab4d552979bf3df343a857
Anubhav0355 <anubhavsingh0355@gmail.com>
2023-04-14 18:45:44 +0530

    Question

---
+++ 1000 and Below/766B_Mahmoud_and_a_Triangle.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define fo(i,n) for(int i=0;i<n;i++)
+#define pb push_back
+#define mp make_pair
+#define all(x) x.begin(), x.end()
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pii> vpii;
+typedef vector<pl> vpl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+
+int main() {
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    
+    int n;
+    cin>>n;
+
+    ll arr[n];
+    fo(i,n) cin>>arr[i];
+
+    sort(arr,arr+n);
+
+    bool isPossible = false;
+
+    for(int i=1; i<n-1; i++) {
+        if(arr[i-1] + arr[i] > arr[i+1]) {
+            isPossible = true;
+            break;
+        }
+    }
+
+    if(isPossible) {
+        cout<<"YES\n";
+    }
+    else {
+        cout<<"NO\n";
+    }
+
+    return 0;
+}
